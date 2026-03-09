38a32fe5140bdcb2d19e145a645216700e5ce4d9
whoIsSudhir <112376748+IamSudhir-Kumar@users.noreply.github.com>
2026-03-09 18:39:58 +0530

    new model with song

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ const onProgress = (event) => {
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+const modelViewer = document.querySelector('model-viewer');
+const audio = document.querySelector('#ar-audio');
+
+modelViewer.addEventListener('ar-status', (event) => {
+  if (event.detail.status === 'session-started') {
+    audio.play();
+  } else if (event.detail.status === 'not-presenting') {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+});
